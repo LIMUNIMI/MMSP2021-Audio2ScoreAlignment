a43546c51089d72c4e41b295c964a1ef39547c4d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-13 18:15:16 +0000

    Adding tarfile member sanitization to extractall()

--- alignment/asmd/asmd/install.py
+++ alignment/asmd/asmd/install.py
@@ -305,7 +305,26 @@ def main():
                 member for member in tf.getmembers() for d in data
                 if d['name'] in member.name
             ]
-            tf.extractall(path=install_dir, members=subdir_and_files)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tf, path=install_dir, members=subdir_and_files)
 
     # saving the Json file as modified
     with open(joinpath(THISDIR, 'datasets.json'), 'w') as fd:
